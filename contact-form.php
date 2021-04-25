a269bb392d6ae65084dcc5e796fc11ee8f8eb3d5
zlim3 <83176830+zlim3@users.noreply.github.com>
2021-04-25 09:15:30 -0700

    Add files via upload

---
+++ contact-form.php
@@ -0,0 +1,23 @@
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $POST['email'];
+  $message = $_POST['message'];
+
+  $email_from = 'zacherylim10s@gmail.com';
+
+  $email_subject = "[From Double Shots Website]"
+
+  $email_body = "User Name: $name.\n"; "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+  $to = "zacherylim3@gmail.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n"
+
+  mail($to, $email_subject, $email_body, $headers);
+
+  header("Location: index.html");
+  
+?>
